8bdafbcc77fa8e7f40e340675995188c093b7c45
Joshua Benjamin <jbenj@sas.upenn.edu>
2020-05-16 12:10:02 -0400

    beginning questions file added

--- dbs/seed.sql
+++ dbs/seed.sql
@@ -9,21 +9,21 @@ VALUES
 INSERT INTO roles
     (title, salary, department_id)
 VALUES
-    ('Sales Lead', 100000, 1),
-    ('Salesperson', 80000, 1),
-    ('Lead Engineer', 150000, 2),
-    ('Software Engineer', 120000, 2),
-    ('Account Manager', 160000, 3),
-    ('Accountant', 125000, 3),
-    ('Legal Team Lead', 250000, 4),
-    ('Lawyer', 190000, 4);
+    ('Head of Sales', 130000, 1),
+    ('Salesperson', 75000, 1),
+    ('Lead Engineer', 140000, 2),
+    ('Software Engineer', 110000, 2),
+    ('Account Manager', 130000, 3),
+    ('Accountant', 135000, 3),
+    ('Legal Team Lead', 275000, 4),
+    ('Lawyer', 200000, 4);
 INSERT INTO employee
     (first_name, last_name, role_id, manager_id)
 VALUES
-    ('John', 'Doe', 1, NULL),
-    ('Mike', 'Chan', 2, 1),
-    ('Ashley', 'Rodriguez', 3, NULL),
-    ('Kevin', 'Tupik', 4, 3),
-    ('Kunal', 'Singh', 5, NULL),
-    ('Malia', 'Brown', 6, 5),
-    ('Sarah', 'Lourd', 7, NULL),
+    ('El Hajj Malik', 'Al Shabazz', 1, NULL),
+    ('Medgar', 'Evers', 2, 1),
+    ('Rosa', 'Parks', 3, NULL),
+    ('Hugo', 'Chavez', 4, 3),
+    ('Mamie', 'Till', 5, NULL),
+    ('Nelson', 'Mandela', 6, 5),
+    ('Bartolome', 'de las Casas', 7, NULL),
---
+++ firstQs.js
@@ -0,0 +1,10 @@
+const firstQs = [
+    {
+        type: "list",
+        name: "starting",
+        message: "Choose how you want to search the database",
+        choices: ["Employee's Department", "Employee's Role", "Employee's Name", "End"]
+    }
+]
+
+module.exports = firstQs;
